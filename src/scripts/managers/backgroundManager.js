bdd0e51a6c0b1d27bd38bedef6fe67d0c6d6ee56
brauliosba <brauliosba07@gmail.com>
2024-07-01 15:03:14 -0500

    Update backgroundManager.js

--- src/scripts/managers/backgroundManager.js
+++ src/scripts/managers/backgroundManager.js
@@ -3,7 +3,7 @@ export class BackgroundManager
     constructor(scene, gameWidth) {
         this.scene = scene;
         this.gameWidth = gameWidth;
-        this.horizontalSpeed = this.scene.toPixels(5);
+        this.horizontalSpeed = this.scene.toPixels(2);
         this.acceleration = this.scene.toPixels(this.scene.data.get('bgAscendantAcceleration'));
         this.maxSpeed = this.scene.toPixels(this.scene.data.get('bgAscendantMaxSpeed'));
         this.backgrounds = [];
@@ -13,7 +13,7 @@ export class BackgroundManager
         this.b1 = this.scene.add.image(0, 0, 'square').setOrigin(0).setDisplaySize(this.gameWidth, this.gameWidth);
         this.backgrounds.push(this.b1);
 
-        this.b2 = this.scene.add.image(0, 0, 'square').setOrigin(0).setDisplaySize(this.gameWidth, this.gameWidth);
+        this.b2 = this.scene.add.image(0, 0, 'square').setOrigin(0).setDisplaySize(this.gameWidth, this.gameWidth).setTint('0x95A922');
         this.b2.x = this.b1.displayWidth;
         this.backgrounds.push(this.b2);
     }
